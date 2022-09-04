9e53b08ad28b3c025500de5ce08a9f8089f61358
abdo-355 <abdoluxor366@gmail.com>
2022-09-04 19:30:23 +0200

    add functional create sream button

--- src/components/streams/streamList.jsx
+++ src/components/streams/streamList.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 const StreamList = ({ fetchStreams, streams, currentUserId, isSignedIn }) => {
@@ -32,10 +33,23 @@ const StreamList = ({ fetchStreams, streams, currentUserId, isSignedIn }) => {
     ));
   };
 
+  const renderCreateButton = () => {
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       <h2>Streams</h2>
       <div className="ui celled list">{renderList()}</div>
+      {renderCreateButton()}
     </div>
   );
 };
